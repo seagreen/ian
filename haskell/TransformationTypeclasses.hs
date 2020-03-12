3e278867bc283c2694f4c0c4e2b02656a341f9be
Ian Grant Jeffries <ian@housejeffries.com>
2020-03-12 19:09:45 -0400

    Rework Selective (thanks @snowleopard)

--- haskell/TransformationTypeclasses.hs
+++ haskell/TransformationTypeclasses.hs
@@ -3,9 +3,10 @@ module TransformationTypeclasses where
 import Control.Category (Category)
 import Control.Comonad (Comonad)
 import Control.Selective (Selective)
+import Data.Function ((&))
 import Data.Functor.Contravariant (Contravariant)
 import Data.Functor.Contravariant.Divisible (Divisible)
-import Prelude (Applicative, Either, Functor, Monad, flip)
+import Prelude (Applicative, Either, Functor, Monad)
 
 import qualified Control.Category as Category
 import qualified Control.Comonad as Comonad
@@ -14,14 +15,14 @@ import qualified Data.Functor.Contravariant as Contravariant
 import qualified Data.Functor.Contravariant.Divisible as Divisible
 import qualified Prelude
 
-($)        ::                     (a ->   b) ->           a    ->        b
-fmap       :: Functor     f =>    (a ->   b) -> f         a    -> f      b
-(<*>)      :: Applicative f =>  f (a ->   b) -> f         a    -> f      b
-(=<<)      :: Monad       f =>    (a -> f b) -> f         a    -> f      b
-extend     :: Comonad     f => (f  a ->   b) -> f         a    -> f      b
-(.)        ::                     (a ->   b) -> (->) x    a    -> (->) x b
-(<<<)      :: Category    c =>  c  a      b  -> c    x    a    -> c    x b
-flipSelect :: Selective   f =>  f (a ->   b) -> f (Either a b) -> f      b
+($)        ::                             (a ->    b) ->        a ->        b
+fmap       :: Functor     f =>            (a ->    b) -> f      a -> f      b
+(<*>)      :: Applicative f =>  f         (a ->    b) -> f      a -> f      b
+(=<<)      :: Monad       f =>            (a -> f  b) -> f      a -> f      b
+extend     :: Comonad     f => (f          a ->    b) -> f      a -> f      b
+(.)        ::                             (a ->    b) -> (->) x a -> (->) x b
+(<<<)      :: Category    c =>  c          a       b  -> c    x a -> c    x b
+swapSelect :: Selective   f =>  f (Either (a -> b) b) -> f      a -> f      b
 
 contramap :: Contravariant f => (b ->  a    ) -> f a        -> f b
 divide    :: Divisible     f => (b -> (a, x)) -> f a -> f x -> f b
@@ -33,7 +34,7 @@ fmap = Prelude.fmap
 extend = Comonad.extend
 (.) = (Prelude..)
 (<<<) = (Category.<<<)
-flipSelect = flip Selective.select
+swapSelect fEither = Selective.select fEither . fmap (&)
 
 contramap = Contravariant.contramap
 divide = Divisible.divide
