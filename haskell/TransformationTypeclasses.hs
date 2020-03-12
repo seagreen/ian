08fb10be8ee90c7cce6a212bf07b1b44e7b506c2
Ian Grant Jeffries <ian@housejeffries.com>
2020-03-12 14:14:49 -0400

    Add comonad and contravariant (thanks @lemastero)

--- haskell/TransformationTypeclasses.hs
+++ haskell/TransformationTypeclasses.hs
@@ -1,19 +1,29 @@
 module TransformationTypeclasses where
 
 import Control.Category (Category)
+import Control.Comonad (Comonad)
+import Data.Functor.Contravariant
 import Prelude (Applicative, Functor, Monad)
 
 import qualified Control.Category as Category
+import qualified Control.Comonad as Comonad
+import qualified Data.Functor.Contravariant as Contravariant
 import qualified Prelude
 
-($)   ::                    (a ->   b) ->     a ->     b
-fmap  :: Functor     f =>   (a ->   b) -> f   a -> f   b
-(<*>) :: Applicative f => f (a ->   b) -> f   a -> f   b
-(=<<) :: Monad       f =>   (a -> f b) -> f   a -> f   b
-(<<<) :: Category    c => c  a      b  -> c x a -> c x b
+($)    ::                     (a ->   b) ->     a ->     b
+fmap   :: Functor     f =>    (a ->   b) -> f   a -> f   b
+(<*>)  :: Applicative f =>  f (a ->   b) -> f   a -> f   b
+(=<<)  :: Monad       f =>    (a -> f b) -> f   a -> f   b
+extend :: Comonad     f => (f a  ->   b) -> f   a -> f   b
+(<<<)  :: Category    c =>  c  a      b  -> c x a -> c x b
+
+contramap :: Contravariant f => (b -> a) -> f a -> f b
 
 ($) = (Prelude.$)
 fmap = Prelude.fmap
 (<*>) = (Prelude.<*>)
 (=<<) = (Prelude.=<<)
+extend = Comonad.extend
 (<<<) = (Category.<<<)
+
+contramap = Contravariant.contramap
